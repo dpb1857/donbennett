64d9e0dfbbefaa56230e6b1338b379800503c634
Don Bennett <dpb@donbennett.org>
2014-01-30 21:46:13 -0800

    Modified bootstrap to run puppet.
    Clone with https url to avoid the query about the host key.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-echo "Hello, world."
+set -x
+apt-get -y install git puppet
+
+cd /opt
+git clone https://github.com/RandonneursUSA/rusa.git
+./rusa/puppet/run_puppet.sh
+
+echo "Finished puppetized setup."
