315aca9a4e69687cd1da2ed251df52ae4f5028b7
Don Bennett <dpb@donbennett.org>
2014-02-03 22:29:17 -0800

    specify shell, sudo group, password hash.

--- puppet/modules/users/manifests/init.pp
+++ puppet/modules/users/manifests/init.pp
@@ -3,7 +3,9 @@ class users {
   user {'dpb':
     ensure => "present",
     managehome => true,
-    groups => ["adm", "admin"],
+    shell => "/bin/bash",
+    groups => ["sudo"],
+    password => '$6$yHOWuyY1$PkwUJoWz0WpmLf3wzsna626Zya6KH6GrAXtw0IIFmazZDWwt.lOaF3Fp86OdH0Y7l/Tvc2/ayV4l5/Hb7RoB/0',
   }
 
   file {"/home/dpb":
