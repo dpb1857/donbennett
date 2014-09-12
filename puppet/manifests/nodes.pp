a00220f01985ad1dc68f7b5de21e1a612bd8fc9b
Don Bennett <dpb@donbennett.org>
2014-09-12 01:01:58 +0000

    match whatever name this node has.

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,6 +1,14 @@
 
-node www {
+node /.*\.com/ {
     include website
     include devtools
     include users
+    include sudoers
+}
+
+node /.*\.org/ {
+    include website
+    include devtools
+    include users
+    include sudoers
 }
