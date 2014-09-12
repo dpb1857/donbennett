7a2fc7c2f74c6f6e71e95819ef62ef0f9faf3b42
Don Bennett <dpb@donbennett.org>
2014-09-12 01:00:48 +0000

    Add emacs build script.

---
+++ misc/build-emacs24.sh
@@ -0,0 +1,12 @@
+set -x
+sudo aptitude install git-core libxaw7-dev libxpm-dev libpng12-dev libtiff5-dev libgif-dev libjpeg8-dev libgtk2.0-dev libncurses5-dev autoconf automake texinfo
+sudo apt-get build-dep emacs
+git clone git://git.savannah.gnu.org/emacs.git
+cd emacs/
+git checkout emacs-24.3
+git checkout -b custom_branch
+./autogen.sh
+./configure --prefix=/opt/emacs24
+make --jobs=2
+sudo make install
+rm -rf emacs
