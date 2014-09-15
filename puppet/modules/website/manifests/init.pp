882ec1098945261cbc4b064db2aae0c4db36338d
Don Bennett <dpb@donbennett.org>
2014-09-15 05:04:44 +0000

    Add nginx configuration.

--- puppet/modules/website/manifests/init.pp
+++ puppet/modules/website/manifests/init.pp
@@ -3,4 +3,17 @@ class website {
     package {'nginx':
 	ensure => installed,
     }
+
+    file { '/usr/share/nginx':
+        ensure => "directory",
+        owner => "rusa",
+        group => "rusa",
+        mode  => 755,
+    }
+
+    file { '/etc/nginx/sites-available/default':
+        source => "puppet:///modules/website/default",
+        owner => "root",
+        group => "root",
+    }
 }
