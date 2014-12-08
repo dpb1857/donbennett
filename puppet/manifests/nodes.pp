8e8e47afc4a47b7d59794b6799e7fb56cbc3700a
Don Bennett <dpb@donbennett.org>
2014-12-07 22:27:20 -0800

    Adaptations for donbennett.org.

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,13 +1,5 @@
 
-node /linode.rusa.org/ {
-    include website
-    include devtools
-    include systools
-    include users
-    include sudoers
-}
-
-node /www.rusa.org/ {
+node /linode2.donbennett.org/ {
     include website
     include devtools
     include systools
