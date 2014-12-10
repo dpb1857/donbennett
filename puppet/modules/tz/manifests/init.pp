855aeec67097bcca1fcf3058ec705717e44f0bac
Don Bennett <dpb@donbennett.org>
2014-12-09 21:35:12 -0800

    More TZ notes.

--- puppet/modules/tz/manifests/init.pp
+++ puppet/modules/tz/manifests/init.pp
@@ -1,6 +1,11 @@
 
 # If installing this module is insufficient, do:
 #   dpkg-reconfigure tzdata
+#
+# Also need to update /etc/localtime, if I knew how to do this with puppet;
+# TZ=PST8PDT
+# echo $TZ > /etc/timezone
+# /bin/cp -f /usr/share/zoneinfo/$TZ /etc/localtime
 
 class tz {
     file {"/etc/timezone":
